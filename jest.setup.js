359ca0d8021b9ff0c4b7c7b9045612629207f7c4
Grogan Kraus <gekbxd@umsystem.edu>
2025-12-13 03:43:59 -0600

    mock api.js to bypass rn import

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,13 @@
-const mockAsyncStorage = {
-  getItem: jest.fn(() => Promise.resolve('mock-token')),
-  setItem: jest.fn(() => Promise.resolve()),
-  removeItem: jest.fn(() => Promise.resolve()),
-  clear: jest.fn(() => Promise.resolve()),
-};
+jest.mock('./src/config/api', () => ({
+  default: 'http://localhost:3000/api'
+}));
 
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('react-native', () => ({
+  Platform: {
+    OS: 'ios',
+    select: jest.fn((obj) => obj.ios || obj.default),
+  },
+}));
 
 jest.mock('expo-constants', () => ({
   default: {
@@ -17,9 +19,14 @@ jest.mock('expo-constants', () => ({
     hostUri: 'localhost:3000'
   }
 }));
-jest.mock('react-native/Libraries/Utilities/Platform', () => ({
-  OS: 'ios',
-  select: jest.fn((obj) => obj.ios || obj.default),
-}));
+
+const mockAsyncStorage = {
+  getItem: jest.fn(() => Promise.resolve('mock-token')),
+  setItem: jest.fn(() => Promise.resolve()),
+  removeItem: jest.fn(() => Promise.resolve()),
+  clear: jest.fn(() => Promise.resolve()),
+};
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 global.AsyncStorage = mockAsyncStorage;
