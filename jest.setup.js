c1e15b37eb3668db1b723177a150fbb9891ec13a
Grogan Kraus <gekbxd@umsystem.edu>
2025-12-13 03:27:38 -0600

    switching it back?

--- jest.setup.js
+++ jest.setup.js
@@ -7,12 +7,19 @@ const mockAsyncStorage = {
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
-
 jest.mock('expo-constants', () => ({
+  default: {
+    expoConfig: {
+      hostUri: 'localhost:3000'
+    }
+  },
   expoConfig: {
     hostUri: 'localhost:3000'
   }
 }));
-
+jest.mock('react-native/Libraries/Utilities/Platform', () => ({
+  OS: 'ios',
+  select: jest.fn((obj) => obj.ios || obj.default),
+}));
 
 global.AsyncStorage = mockAsyncStorage;
